8829f27624056e9b86feacfbe9a58a5d4ccf27a3
Filip <filipblazevski00@hotmail.com>
2023-04-24 18:51:52 +0200

    projects sections aos

--- src/components/Project/Project.tsx
+++ src/components/Project/Project.tsx
@@ -1,15 +1,20 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./Project.css";
 import { ProjectDetails } from "../../interfaces/ProjectDetails";
+import "aos/dist/aos.css";
+import aos from "aos";
 
 type ProjectProps = {
   project: ProjectDetails;
 };
 
 export const Project = ({ project }: ProjectProps) => {
+  useEffect(() => {
+    aos.init({ duration: 1000 });
+  }, []);
   return (
     <>
-      <div className="project-container">
+      <div className="project-container" data-aos="fade-left">
         <div className="project-thumbnail">
           <img
             src="https://img.freepik.com/free-photo/digital-painting-mountain-with-colorful-tree-foreground_1340-25699.jpg?w=1380&t=st=1682338334~exp=1682338934~hmac=e206aa4ac4d054e29a23ac6879790149ffb830379b5afbfde5271c38d61005b2"
--- src/components/Projects/Projects.tsx
+++ src/components/Projects/Projects.tsx
@@ -1,16 +1,24 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { ProjectDetails } from "../../interfaces/ProjectDetails";
 import { Project } from "../Project/Project";
+import "./Projects.css";
+import "aos/dist/aos.css";
+import Aos from "aos";
 
 type ProjectsProps = {
   projects: ProjectDetails[];
 };
 
 export const Projects = ({ projects }: ProjectsProps) => {
+  useEffect(() => {
+    Aos.init({ duration: 2000 });
+  }, []);
   return (
     <>
       <div id="projects">
-        <div className="section-title">Projects</div>
+        <div className="section-title" data-aos="fade-right">
+          Projects
+        </div>
         {projects.map((p, i) => (
           <div className="project-wrapper" key={i}>
             <Project project={p} />
